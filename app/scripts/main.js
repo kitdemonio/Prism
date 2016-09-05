db890386cc4c90b6c822bf797479e4c1adfd7c11
kitdemonio <ddd48533@gmail.com>
2016-09-05 15:37:02 +0200

    Add new section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,4 +3,35 @@ $(document).ready(function () {
   $(function(){
     $('#nav').slicknav();
   });
+
+  $("#owl-carousel").owlCarousel({
+    singleItem: true,
+
+    // navigation: true,
+    navigationText: false,
+    // responsive: true,
+    pagination: true,
+  });
+
+
+
+  // Limit words in article paragraph
+  $('p[data-words-limit]').map(function(i,el){
+    var limit = +el.dataset.wordsLimit;
+    var array = el.innerText.split(' ');
+    array.length = limit;
+    array.push('...');
+    el.innerText = array.join(' ');
+  });
+
+  // Anchor scroll
+  $('a[href^="#"]').click(function(){
+    var el = $(this).attr('href');
+    $('li>.active').removeClass('active');
+    $(this).addClass('active');
+    $('body').animate({
+      scrollTop: $(el).offset().top}, 800);
+    return false;
+  });
+
 });
