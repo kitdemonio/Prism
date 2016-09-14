acd7804340c0e01ce0f8ab0a992a21af93696d66
kitdemonio <ddd48533@gmail.com>
2016-09-14 22:38:58 +0200

    Add search

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,4 @@
 'use strict';
-// var $ = require('jQuery');
 
 $(document).ready(function () {
   // Initialise adaptive nav
@@ -30,11 +29,35 @@ $(document).ready(function () {
   $('a[href^="#"][href!="#"]').click(function () {
     var el = $(this).attr('href');
     $('li>.active').removeClass('active');
-      $(this).addClass('active');
+    $(this).addClass('active');
     $('body').animate({
       scrollTop: $(el).offset().top
     }, 800);
     return false;
+  });
+
+  //Modal popup
+  $('#search').on('click', function () {
+    var regex = new RegExp(/\d+/);
+    var box = $('.search-box');
+    var input = $('input[type="search"]');
+
+    $('body').css('position', 'relative');
+    input.addClass('modal-search');
+    box.fadeIn(500);
+    box.css('height',window.innerHeight);
+    input.fadeIn(500);
+    input.css('top',((window.innerHeight - regex.exec(input.css('height'))[0]) / 2));
+    input.css('left',((window.innerWidth - regex.exec(input.css('width'))[0]) / 2));
+
+
+    box.on('click', function (e) {
+      if (e.target.nodeName != "INPUT") {
+        box.fadeOut(500);
+        input.fadeOut(500);
+      }
+    });
+
   });
 
 });
