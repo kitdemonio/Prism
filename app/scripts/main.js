792b1d1ae6d9bec28e43227c521da6162263a6cc
kitdemonio <ddd48533@gmail.com>
2016-09-16 01:46:00 +0200

    Add search & Fixes for IE10+

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 $(document).ready(function () {
   // Initialise adaptive nav
   $(function () {
@@ -8,10 +9,7 @@ $(document).ready(function () {
 
   $('#owl-carousel').owlCarousel({
     singleItem: true,
-
-    // navigation: true,
     navigationText: false,
-    // responsive: true,
     pagination: true
   });
 
@@ -26,38 +24,66 @@ $(document).ready(function () {
   });
 
   // Anchor scroll
-  $('a[href^="#"][href!="#"]').click(function () {
-    var el = $(this).attr('href');
-    $('li>.active').removeClass('active');
-    $(this).addClass('active');
-    $('body').animate({
-      scrollTop: $(el).offset().top
-    }, 800);
-    return false;
-  });
-
-  //Modal popup
-  $('#search').on('click', function () {
-    var regex = new RegExp(/\d+/);
-    var box = $('.search-box');
-    var input = $('input[type="search"]');
-
-    $('body').css('position', 'relative');
-    input.addClass('modal-search');
-    box.fadeIn(500);
-    box.css('height',window.innerHeight);
-    input.fadeIn(500);
-    input.css('top',((window.innerHeight - regex.exec(input.css('height'))[0]) / 2));
-    input.css('left',((window.innerWidth - regex.exec(input.css('width'))[0]) / 2));
+  if (navigator.sayswho !== 'IE 11') { // Because in IE 11 anchor scrolling doesn't work
+    $('a[href^="#"][href!="#"]').click(addAnchorScroll);
+  }
 
 
-    box.on('click', function (e) {
-      if (e.target.nodeName != "INPUT") {
-        box.fadeOut(500);
-        input.fadeOut(500);
-      }
-    });
-
-  });
-
+  //Modal popup for all browsers except IE10
+  $('#search').on('click', addSearchPopup);
 });
+
+var search = document.getElementById('search');
+if (search.attachEvent) {
+  search.attachEvent('onclick', addSearchPopup); //Search popup for IE10
+}
+
+function addSearchPopup() {
+  var regex = new RegExp(/\d+/);
+  var box = $('.search-box');
+  var input = $('input[type="search"]');
+
+
+  $('body').css('position', 'relative');
+  input.addClass('modal-search');
+  box.fadeIn(500);
+  box.css('height',window.innerHeight);
+  input.fadeIn(500);
+  input.css('top',((window.innerHeight - regex.exec(input.css('height'))[0]) / 2));
+  input.css('left',((window.innerWidth - regex.exec(input.css('width'))[0]) / 2));
+
+
+  box.on('click', function (e) {
+    if (e.target.nodeName != "INPUT") {
+      box.fadeOut(500);
+      input.fadeOut(500);
+    }
+  });
+}
+
+function addAnchorScroll(e) {
+  var el = $(this).attr('href');
+  $('li>.active').removeClass('active');
+  $(this).addClass('active');
+  e.preventDefault();
+  $('body').animate({
+    scrollTop: $(el).offset().top
+  }, 800);
+  return false;
+}
+
+navigator.sayswho= (function(){ // Browser detection
+  var ua= navigator.userAgent, tem,
+    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+  if(/trident/i.test(M[1])){
+    tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+    return 'IE '+(tem[1] || '');
+  }
+  if(M[1]=== 'Chrome'){
+    tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+    if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+  }
+  M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+  if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+  return M.join(' ');
+})();
